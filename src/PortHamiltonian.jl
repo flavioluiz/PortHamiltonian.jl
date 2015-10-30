d460536fca055d2f88522c610d98496d27bf3620
Flávio Ribeiro <flavioluiz@gmail.com>
2015-10-30 19:47:15 +0100

    updated structure

---
+++ src/PortHamiltonian.jl
@@ -0,0 +1,42 @@
+# * higher order? rdiff? ctaylor? audi?
+# * moulla2
+# * test cases: fluid, beam
+# * Hamiltonian function -> use forwarddiff for gradient and Hessian?
+# * automatic coupling phs?
+# * better way to simulate?
+# * automatic subdivide in elements?
+
+module PortHamiltonian
+
+using ForwardDiff
+
+include("lgwt.jl")
+include("derivative.jl")
+include("massmatrix.jl")
+include("phdiscretization.jl")
+
+function leg_pol(x :: Array,xi :: Array,j :: Integer)
+	leg_pol(x[1],xi,j)
+end
+function leg_pol(x :: Number,xi :: Array,j :: Integer)
+	# evaluate the "j" Lagrange polynomial defined using
+    # the collocation points "xi" at point "x"
+	P = 1.;
+        for k = 1:length(xi)
+	        if (k != j)
+		        P = P*(x[1]-xi[k])/(xi[j]-xi[k])
+            end
+        end
+    return P
+end
+
+type Phs
+	J;
+	B;
+	D;
+	wi;
+    zi;
+	Q;
+end
+
+end
---
+++ src/phdiscretization.jl
@@ -0,0 +1,39 @@
+function discrete_phs(N,a,b)
+	Nz = N
+	Nx = Nz+1
+	xi,wi = lgwt(Nx,a,b)
+	if Nz > 1
+		zi,wiz = lgwt(Nz,a,b)
+	else
+		zi,wiz = (a+b)/2, (b-a);
+	end
+	M = massmatrix(xi,zi,wi);
+	D = dermatrix(xi,zi,1);
+	p0 = map(i->leg_pol(a,xi,i), 1:length(xi))
+	pL = map(i->leg_pol(b,xi,i), 1:length(xi))
+	
+	F = [M 0*M;
+	     pL' pL'*0;
+		 0*M M;
+		 0*p0' p0'];
+	E = -[D*0 D;
+	      pL'*0 -pL';
+		  D D*0;
+		  p0' p0'*0];
+		  J = E/F
+    phJ = -[zeros(N,N) J[1:N,(N+2):end-1];
+		          J[N+2:end-1,1:N] zeros(N,N)];
+    phB = -[J[[1:N;N+2:2*N+1],N+1] J[[1:N;(N+2):(2*N+1)],end]];
+    phD = [J[N+1,N+1] J[N+1,end];
+        J[end,N+1] J[end,end]];
+	phw = wiz;
+	phzi = zi;
+	if N > 1
+		Q = sparse(diagm(wiz[:],0));
+		Q = blkdiag(Q,Q);
+		else
+		Q = diagm([wiz; wiz][:]);
+	end
+	
+	return Phs(phJ, phB, phD, phw,phzi,Q)
+end
